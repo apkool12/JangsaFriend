6419a047a8bd382284e14e6c565fedfde638c4df
dmd-sik <apkool12@naver.com>
2025-10-21 16:04:30 +0900

    [Fix] - Resolve TypeScript and Metro configuration issues

--- metro.config.js
+++ metro.config.js
@@ -2,14 +2,19 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
+// SVG transformer 설정
 config.transformer.babelTransformerPath = require.resolve(
   "react-native-svg-transformer"
 );
 
+// SVG 파일 처리 설정
 config.resolver.assetExts = config.resolver.assetExts.filter(
   (ext) => ext !== "svg"
 );
 
 config.resolver.sourceExts = [...config.resolver.sourceExts, "svg"];
+
+// TypeScript 설정 개선
+config.resolver.platforms = ["ios", "android", "native", "web"];
 
 module.exports = config;
